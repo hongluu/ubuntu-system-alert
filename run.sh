0305721cf366ff653c0a0cc8cd4ed4587f27c1ee
hong luu <honglm1011@gmail.com>
2020-12-10 10:54:40 +0700

    Update run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-bash server-monitoring.sh --debug=true --hostname=fs.app --from=server@yourdomain.com --to=admin@yourdomain.com --cpu=warning=20:critical=50 --memory=warning=30:critical=60 --disk=warning=40:critical=60:fatal=70 
+bash check_alert.sh --debug=true --hostname=fs.app --from=server@yourdomain.com --to=admin@yourdomain.com --cpu=warning=20:critical=50 --memory=warning=30:critical=60 --disk=warning=40:critical=60:fatal=70 
